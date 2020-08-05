a14454ffe83690026402f99be56acec65f09fa44
Helena Kopp <helena.mkopp@gmail.com>
2020-08-05 17:53:05 -0300

    dia tres-

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import whatsIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher {
     id: number;
@@ -19,6 +20,11 @@ interface TeacherItemProps {
 }
 
 const TeacherItem:React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
   return (
     <article className="teacher-item">
       <header>
@@ -33,7 +39,7 @@ const TeacherItem:React.FC<TeacherItemProps> = ({ teacher }) => {
       <footer>
         <p>Preço/hora
             <strong>R$ {teacher.cost}</strong> </p>
-        <a href={`https://wa.me/${teacher.whatsapp}`}>
+        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsIcon} alt="Whatsapp" />
               Entrar em contato
             </a>
